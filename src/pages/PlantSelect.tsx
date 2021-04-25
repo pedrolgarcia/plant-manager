c3e218d5fae2af1221fd85bbb69ea9778bbad393
Pedro Lopes <pedrolopes.info@gmail.com>
2021-04-25 01:37:14 -0300

    feat: fake api, environments api integration

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, FlatList } from 'react-native'
 
 import Header from '../components/Header'
@@ -7,7 +7,25 @@ import EnvironmentButton from '../components/EnvironmentButton'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
+import api from '../services/api'
+
+interface EnvironmentsProps {
+  key: string;
+  title: string;
+}
+
 export default function PlantSelect() {
+  const [environments, setEnvironments] = useState<EnvironmentsProps[]>([]);
+
+  useEffect(() => {
+    async function fetchEnvironments() {
+      const { data } = await api.get('plants_environments');
+      setEnvironments([{ key: 'all', title: 'Todos' }, ...data]);
+    }
+
+    fetchEnvironments();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -19,9 +37,9 @@ export default function PlantSelect() {
 
       <View>
         <FlatList
-          data={[1, 2, 3, 4, 5, 6]}
+          data={environments}
           renderItem={({ item }) => (
-            <EnvironmentButton title="Cozinha" />
+            <EnvironmentButton title={item.title} />
           )}
           horizontal
           showsHorizontalScrollIndicator={false}
